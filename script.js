10eedd73f3e62a4f1dbab2187389ab6100469873
M.vikram <101389124+vikram0411@users.noreply.github.com>
2024-03-30 13:12:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Java Backend Developer", "Embedded Software Engineer" ,],
+        strings: ["Embedded Software Engineer" ,],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Java Backend Developer", "Embedded Software Engineer",],
+        strings: ["Embedded Software Engineer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
